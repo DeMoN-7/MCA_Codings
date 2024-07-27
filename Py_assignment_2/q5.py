2b20340e59b1705d6c219570b700e1d47f48712f
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-07-27 17:06:45 +0530

    q5

---
+++ Py_assignment_2/q5.py
@@ -0,0 +1,14 @@
+# Question 5: Count Occurrences of a Character in a String 
+# Task: Write a program that takes a string and a character as input from the user and counts 
+# the number of occurrences of the character in the string using a loop and conditional 
+# statements. Print the count. 
+
+a=input("Enter the string ")
+a=a.capitalize()
+ch=input("Enter char ")
+ch=ch.capitalize()
+count=0
+for c in a:
+    if c==ch:
+        count+=1
+print(ch," occurs ",count," times")
