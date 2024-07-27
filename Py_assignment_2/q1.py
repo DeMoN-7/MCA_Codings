21af71a0fc279784550365da3d8f6a4ee330867b
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-07-27 15:44:42 +0530

    commit

---
+++ Py_assignment_2/q1.py
@@ -0,0 +1,12 @@
+# Question 1: Check for Vowels 
+# Task: Write a program that checks if a given character is a vowel or consonant. The program 
+# should take a character input from the user and use conditional statements to determine and 
+# print whether it is a vowel or consonant.
+
+
+a=input("Enter Character")
+lis=['a','e','i','o','u']
+if a in lis:
+    print(a," is vowel")
+else:
+    print(a," is consonant")
