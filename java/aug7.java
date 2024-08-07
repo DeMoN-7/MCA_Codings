878f3f865c3312971dd7db729b658f1c248da7a9
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-08-07 10:16:26 +0530

    just class work

---
+++ java/aug7.java
@@ -0,0 +1,9 @@
+public class aug7 {
+    public void fun(int n){
+        System.out.println("printing from function fun: "+n);
+    }
+    public static void main(String[] args) {
+        aug7 ag=new aug7();
+        ag.fun(225);
+    }
+}
