ee6ceb8be8166cb6351572b5008014e9ee01d4f6
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-08-07 15:15:03 +0530

    changes in code

--- java/aug7.java
+++ java/aug7.java
@@ -1,6 +1,9 @@
 public class aug7 {
+    aug7(){
+        System.out.println("Constructor called\n\n");
+    }
     public void fun(int n){
-        System.out.println("printing from function fun: "+n);
+        System.out.println("printed from function: "+n);
     }
     public static void main(String[] args) {
         aug7 ag=new aug7();
