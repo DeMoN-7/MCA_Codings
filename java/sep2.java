4197f31aef11976091cd8595fb20762ca566dc56
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-04 10:30:12 +0530

    using interface

--- java/sep2.java
+++ java/sep2.java
@@ -1,11 +1,22 @@
 interface temp{
    default void prin(){
-        System.out.println("from interface");
+        System.out.println("from interface 1 ");
     }
 }
-class tempp implements temp{
+interface temp1 extends temp{
+
+   default void prin(){
+        temp.super.prin();
+        System.out.println("from interface 2 ");
+    }
+}
+class tempp implements temp1{
     public void info(){
         System.out.println("form temp");
+    }
+    public void prin(){
+        temp1.super.prin();
+        System.out.println("form temp prin method");
     }
 }
 public class sep2  {
@@ -14,6 +25,7 @@ public class sep2  {
         tempp tt= new tempp();
         tt.info();
         tt.prin();
+        // tt.prin1();
         
     }   
 }
