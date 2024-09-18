67f5596d26dabdc926ef981b9476ac726765ea61
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-18 12:01:29 +0530

    getting handy with split function

--- java/unit-3 test practice/splitFunction.java
+++ java/unit-3 test practice/splitFunction.java
@@ -1,8 +1,13 @@
 public class splitFunction {
 public static void main(String[] args) {
-    String str="hello@ayush.singh.in,@okmail";
-    String []res=str.split("@");
+    String str="hello@ayush.,singh.in,@okmail@@";
+    String []res=str.split("@",-3);
     for (String string : res) {
+        System.out.println(string);
+    }
+    System.out.println(".....");
+    String []res1=str.split(",");
+    for (String string : res1) {
         System.out.println(string);
     }
 }    
