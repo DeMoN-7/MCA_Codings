27b0bec7c367c4f9007b1666054db3aa775d5138
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-18 10:10:51 +0530

    Simple string manuplation code

---
+++ java/unit-3 test practice/string1.java
@@ -0,0 +1,10 @@
+public class string1 {
+public static void main(String[] args) {
+    String ss="Hello";
+    ss=ss.concat(" we");
+    System.out.println(ss);
+    ss="welcome";
+    System.out.println(ss);
+    
+}    
+}
