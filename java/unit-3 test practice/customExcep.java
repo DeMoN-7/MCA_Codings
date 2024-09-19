7c63ebf5a876c49911b52e7673256514a925fac0
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-19 12:37:53 +0530

    custom exception

--- java/unit-3 test practice/customExcep.java
+++ java/unit-3 test practice/customExcep.java
@@ -1,3 +1,31 @@
+import java.util.*;
+
+class excepCustom extends Exception {
+    public excepCustom(String mess) {
+        super(mess);
+    }
+}
+
 public class customExcep {
-    
+    public static void checkAge(int age) throws excepCustom {
+        if (age < 18) {
+            throw new excepCustom("Grow up kid");
+        } else {
+            System.out.println("good");
+        }
+
+    }
+
+    public static void main(String[] args) {
+       try{
+        checkAge(19);
+        checkAge(21);
+        checkAge(2);
+       }
+       catch(excepCustom e){
+        System.out.println(e);
+       }
+        
+        
+    }
 }
