7a420aec1eed83c62e7ef66b429b9302eb18a33f
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-19 11:28:11 +0530

    learning throw keyword

--- java/unit-3 test practice/throww.java
+++ java/unit-3 test practice/throww.java
@@ -1,3 +1,15 @@
 public class throww {
-    
+    static void check(int z){
+        if (z<18){
+            throw new ArithmeticException("Age must be above 18");
+
+        }
+        else{
+            System.out.println("welcome");
+        }
+    }
+    public static void main(String[] args) {
+        check(19);
+        check(12);
+    }
 }
