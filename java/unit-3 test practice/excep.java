4f6939b91e342368995287ae52d68599f1d56ca8
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-09-19 11:25:12 +0530

    try catch block

--- java/unit-3 test practice/excep.java
+++ java/unit-3 test practice/excep.java
@@ -1,3 +1,21 @@
 public class excep {
-    
+    public static void main(String[] args) {
+        try{
+            int a=10/0;
+            System.exit(0);
+            int []a1={1,2,3};
+            System.out.println(a1[4]);
+
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        finally{
+            System.out.println("Finally");
+        }
+        // catch(ArrayIndexOutOfBoundsException e){
+        //     System.out.println("Array");
+        // }
+        
+    }
 }
