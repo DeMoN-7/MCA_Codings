029421e374e4998154519a87e76fad05a6f9d8a5
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-10-07 01:11:44 +0530

    commit

--- java/Assignment-1/airport.java
+++
@@ -1,5 +0,0 @@
-public class airport {
-    public static void main(String[] args) {
-        
-    }
-}
---
+++ java/Assignment-1/airportMain.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class airport{
+    // private attributes
+    private String name;
+    private List<flight>flights =new ArrayList<>();
+    // private methods
+    private boolean findFlight(int flightNumber){
+        return false;
+    }
+    // public methods
+    public void addFlight(){
+
+    }
+    public void removeFlight(int flightNumber){
+
+    }
+    public List<flight>
+
+    
+}
+class flight{
+
+}
+public class airportMain {
+    public static void main(String[] args) {
+        
+    }
+}
