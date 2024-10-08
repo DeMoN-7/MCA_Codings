8912046bb1288fefb020df044238c8d845483ba4
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-10-08 18:56:44 +0530

    commit

--- java/Assignment-1/airportMain.java
+++ java/Assignment-1/airportMain.java
@@ -1,172 +1,172 @@
-import java.time.LocalTime;
-import java.util.ArrayList;
-import java.util.List;
+    import java.time.LocalTime;
+    import java.util.ArrayList;
+    import java.util.List;
 
-class Flight {
+    class Flight {
 
-    // private attributes
-    private static int flightNumberCounter = 1000;
-    private int flightNumber;
-    private String destination;
-    private LocalTime departureTime;
-    private LocalTime ArrivalTime;
+        // private attributes
+        private static int flightNumberCounter = 1000;
+        private int flightNumber;
+        private String destination;
+        private LocalTime departureTime;
+        private LocalTime ArrivalTime;
 
-    // Constructor to assign values
-    Flight(String destination, LocalTime arrival, LocalTime depart) {
-        this.ArrivalTime = arrival;
-        this.departureTime = depart;
-        this.destination = destination;
-        this.flightNumber = ++flightNumberCounter;
+        // Constructor to assign values
+        Flight(String destination, LocalTime arrival, LocalTime depart) {
+            this.ArrivalTime = arrival;
+            this.departureTime = depart;
+            this.destination = destination;
+            this.flightNumber = ++flightNumberCounter;
+        }
+
+        // private methods
+        boolean upcomingFlight() {
+            return departureTime.isAfter(LocalTime.now());
+            // this function return true if departure time is more than current time
+
+        }
+        public boolean isComplete(){
+            return ArrivalTime.isBefore(LocalTime.now());
+            // this method return true if arrival time is less than current time
+        }
+        public int getFlightNumber(){
+            return flightNumber; 
+        }
+        // public method
+        public void updateDeparture(LocalTime newDepart) {
+            // updated departure time only get updated whn it is called
+            this.departureTime = newDepart;
+        }
+
+        public void updateArrival(LocalTime newArrival) {
+            // updated arrival time only get updated whn it is called
+            this.ArrivalTime = newArrival;
+        }
+
+        public void display() {
+            // to display details of flight,including flight number,destination and times
+            System.out.println("Flight Number " + this.flightNumber);
+            System.out.println("Flight Destination " + this.destination);
+            System.out.println("Flight Departure time " + this.departureTime);
+            System.out.println("Flight Arrival time " + this.ArrivalTime);
+        }
+
     }
 
-    // private methods
-    boolean upcomingFlight() {
-        return departureTime.isAfter(LocalTime.now());
-        // this function return true if departure time is more than current time
+    class airport {
+        // private attributes
+        private String name;
+        private List<Flight> flights;
 
-    }
-    public boolean isComplete(){
-        return ArrivalTime.isBefore(LocalTime.now());
-        // this method return true if arrival time is less than current time
-    }
-    public int getFlightNumber(){
-        return flightNumber; 
-    }
-    // public method
-    public void updateDeparture(LocalTime newDepart) {
-        // updated departure time only get updated whn it is called
-        this.departureTime = newDepart;
-    }
+        // Constructor to get value
+        airport(String name){
+            this.name=name;
+            this.flights=new ArrayList<>();
+        }
 
-    public void updateArrival(LocalTime newArrival) {
-        // updated arrival time only get updated whn it is called
-        this.ArrivalTime = newArrival;
-    }
+        // private methods
+        private Flight findFlightByNumber(int flightNumber){
+            for(Flight flight:flights){
+                if(flight.getFlightNumber()==flightNumber){
+                    return flight;
+                }
+            }
+            return null;
+        }
 
-    public void display() {
-        // to display details of flight,including flight number,destination and times
-        System.out.println("Flight Number " + this.flightNumber);
-        System.out.println("Flight Destination " + this.destination);
-        System.out.println("Flight Departure time " + this.departureTime);
-        System.out.println("Flight Arrival time " + this.ArrivalTime);
-    }
 
-}
 
-class airport {
-    // private attributes
-    private String name;
-    private List<Flight> flights;
+        // public methods
+        public void addFlight(Flight flight) {
+            flights.add(flight);
+            // adds flight object to array list or add flight to airport
+        }
 
-    // Constructor to get value
-    airport(String name){
-        this.name=name;
-        this.flights=new ArrayList<>();
-    }
+        public void removeFlight(int flightNumber) {
+            Flight flight=findFlightByNumber(flightNumber);
+            if (flight!=null) {
+                flights.remove(flight);
+                System.out.println("Flight Number: "+flightNumber+" removed");
+            }
+            else{
+                System.out.println("Flight Not Found");
+            }
 
-    // private methods
-    private Flight findFlightByNumber(int flightNumber){
-        for(Flight flight:flights){
-            if(flight.getFlightNumber()==flightNumber){
-                return flight;
+        }
+
+        public List<Flight> upcomingFlights() {
+            List<Flight>upcomingFlights=new ArrayList<>();
+            for(Flight flight:flights){
+                if(flight.upcomingFlight()){
+                    upcomingFlights.add(flight);
+                }
+            }
+            return upcomingFlights;
+        }
+
+        public List<Flight> CompletedFlights() {
+            List<Flight>completedFlights=new ArrayList<>();
+            for(Flight flight:flights){
+                if(flight.isComplete()){
+                    completedFlights.add(flight);
+                }
+            }
+            return completedFlights;
+        }
+
+        public void displayDetails() {
+            System.out.println("All flights at "+name+": ");
+            for(Flight flight:flights){
+                flight.display();
             }
         }
-        return null;
-    }
-
-
-
-    // public methods
-    public void addFlight(Flight flight) {
-        flights.add(flight);
-        // adds flight object to array list or add flight to airport
-    }
-
-    public void removeFlight(int flightNumber) {
-        Flight flight=findFlightByNumber(flightNumber);
-        if (flight!=null) {
-            flights.remove(flight);
-            System.out.println("Flight Number: "+flightNumber+" removed");
-        }
-        else{
-            System.out.println("Flight Not Found");
-        }
 
     }
 
-    public List<Flight> upcomingFlights() {
-        List<Flight>upcomingFlights=new ArrayList<>();
-        for(Flight flight:flights){
-            if(flight.upcomingFlight()){
-                upcomingFlights.add(flight);
+    public class airportMain {
+        public static void main(String[] args) {
+
+            // Creating object for Airport
+            airport airport=new airport("Demonic Ports");
+
+            // Creating object for flights
+            Flight flight1 = new Flight("New York", LocalTime.of(23, 30), LocalTime.of(8, 30));
+            Flight flight2 = new Flight("London", LocalTime.of(15, 0), LocalTime.of(20, 0));
+            Flight flight3 = new Flight("Paris", LocalTime.of(10, 0), LocalTime.of(12, 0));
+            Flight flight4 = new Flight("India", LocalTime.of(3, 01), LocalTime.of(2, 0));
+
+            // adding flights to airport
+            airport.addFlight(flight1);
+            airport.addFlight(flight2);
+            airport.addFlight(flight3);
+            airport.addFlight(flight4);
+
+            // displaying all flight which are at airport
+            airport.displayDetails();
+
+            // updating arrival time of flight 1
+            flight1.updateArrival(LocalTime.of(16, 10));
+            System.out.println("\nAfter updating arrival time \n");
+            airport.displayDetails();
+
+            // removing flight from airport
+            airport.removeFlight(flight2.getFlightNumber());
+
+            // displaying remaining flight
+            System.out.println("\nAfter removing flight 2\n");
+            airport.displayDetails();
+
+            // list of upcoming flights based on current time 
+            System.out.println("\nList of upcoming flights\n");
+            for(Flight flight:airport.upcomingFlights()){
+                flight.display();
             }
-        }
-        return upcomingFlights;
-    }
 
-    public List<Flight> CompletedFlights() {
-        List<Flight>completedFlights=new ArrayList<>();
-        for(Flight flight:flights){
-            if(flight.isComplete()){
-                completedFlights.add(flight);
+            // list of completed flights
+            System.out.println("\n List of completed flights\n");
+            for(Flight flight:airport.CompletedFlights()){
+                flight.display();
             }
-        }
-        return completedFlights;
-    }
 
-    public void displayDetails() {
-        System.out.println("All flights at "+name+": ");
-        for(Flight flight:flights){
-            flight.display();
         }
     }
-
-}
-
-public class airportMain {
-    public static void main(String[] args) {
-
-        // Creating object for Airport
-        airport airport=new airport("Demonic Ports");
-
-        // Creating object for flights
-        Flight flight1 = new Flight("New York", LocalTime.of(23, 30), LocalTime.of(8, 30));
-        Flight flight2 = new Flight("London", LocalTime.of(15, 0), LocalTime.of(20, 0));
-        Flight flight3 = new Flight("Paris", LocalTime.of(10, 0), LocalTime.of(12, 0));
-        Flight flight4 = new Flight("India", LocalTime.of(3, 01), LocalTime.of(2, 0));
-
-        // adding flights to airport
-        airport.addFlight(flight1);
-        airport.addFlight(flight2);
-        airport.addFlight(flight3);
-        airport.addFlight(flight4);
-
-        // displaying all flight which are at airport
-        airport.displayDetails();
-
-        // updating arrival time of flight 1
-        flight1.updateArrival(LocalTime.of(16, 10));
-        System.out.println("\nAfter updating arrival time \n");
-        airport.displayDetails();
-
-        // removing flight from airport
-        airport.removeFlight(flight2.getFlightNumber());
-
-        // displaying remaining flight
-        System.out.println("\nAfter removing flight 2\n");
-        airport.displayDetails();
-
-        // list of upcoming flights based on current time 
-        System.out.println("\nList of upcoming flights\n");
-        for(Flight flight:airport.upcomingFlights()){
-            flight.display();
-        }
-
-        // list of completed flights
-        System.out.println("\n List of completed flights\n");
-        for(Flight flight:airport.CompletedFlights()){
-            flight.display();
-        }
-
-    }
-}
