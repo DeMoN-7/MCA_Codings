5ac410847efa7b91e88f125413598f5c88cadc66
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-10-13 19:25:29 +0530

    comm

---
+++ java/method_overRiding.java
@@ -0,0 +1,17 @@
+class animal{
+    void eat(){
+        System.out.println("animal");
+    }
+}
+class dog extends animal{
+    void eat(){
+        System.out.println("dog");
+    }
+}
+public class method_overRiding {
+    public static void main(String[] args) {
+        animal a1=new dog();
+        a1.eat();
+        
+    }
+}
