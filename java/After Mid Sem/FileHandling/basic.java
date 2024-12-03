e82341e163d811b0fc3cb026981e42ec4bb9744b
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-12-04 02:58:09 +0530

    commit

--- java/After Mid Sem/FileHandling/basic.java
+++ java/After Mid Sem/FileHandling/basic.java
@@ -1,5 +1,27 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class basic {
     public static void main(String[] args) {
+        String inputFile="input.txt";
+        String outputFile="output.txt";
+
+        try{
+
+        InputStream inputStream = new FileInputStream(inputFile);
+        OutputStream outputStream = new FileOutputStream(outputFile);
+        byte [] buffer = new byte[1024];
+        int byter;
+        while ((byter=inputStream.read(buffer))!=-1) {
+            outputStream.write(buffer,0,byter);
+        }
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
         
-    }    
+
+    }
 }
