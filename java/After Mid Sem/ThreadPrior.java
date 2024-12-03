55b9cfc2273d18b891af3534c41b66d54389726a
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-12-03 23:57:33 +0530

    commit

--- java/After Mid Sem/ThreadPrior.java
+++ java/After Mid Sem/ThreadPrior.java
@@ -1,4 +1,22 @@
+class thr extends Thread {
+    thr(String name) {
+        super(name);
+    }
+
+    public void run() {
+        System.out.println("Thread name " + this.getName());
+    }
+}
 
 public class ThreadPrior {
-    
+    public static void main(String[] args) {
+        thr t1 = new thr("Thread 1");
+        thr t2 = new thr("Thread 2");
+        thr t3 = new thr("Thread 3");
+        thr t4 = new thr("Thread 4");
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
 }
