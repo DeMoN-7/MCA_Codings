7d7c0120cfd7e365a1addb2fb61b131cb03f3ff7
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-12-03 15:34:50 +0530

    commit

--- java/After Mid Sem/Thread_runnable.java
+++ java/After Mid Sem/Thread_runnable.java
@@ -1,3 +1,23 @@
+class bullet1 implements Runnable{
+    public void run(){
+        for (int i=0;i<100;i++)
+        System.out.println("Bullet 1..............");
+    }
+}
+class bullet2 implements Runnable{
+    public void run(){
+        for (int i=0;i<100;i++)
+        System.out.println("Bullet2**************");
+    }
+}
 public class Thread_runnable {
-    
+    public static void main(String[] args) {
+        bullet1 bull1=new bullet1();
+        Thread t1=new Thread(bull1);
+        bullet2 bull2=new bullet2();
+        Thread t2=new Thread(bull2);
+
+        t1.start();
+        t2.start();
+    }
 }
