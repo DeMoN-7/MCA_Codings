73f9eb66264e337d9946812ccdcfd6f459b1f032
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-12-04 00:08:29 +0530

    commit

--- java/After Mid Sem/threadMeth.java
+++ java/After Mid Sem/threadMeth.java
@@ -1,10 +1,16 @@
-class thre extends Thread{{
-    public void run(){
-
+class thre extends Thread{
+    thre(String name){
+        super(name);
     }
-}}
+    public void run(){
+        for(int i=0;i<50;i++)
+        System.out.println("hello"+this.getName());
+    }
+}
 public class threadMeth {
     public static void main(String[] args) {
+        thre t1=new thre("Thread 1");
+        thre t2=new thre("Thread 2");
         
     }    
 }
