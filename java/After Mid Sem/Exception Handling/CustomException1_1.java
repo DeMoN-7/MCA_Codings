9247ef408056e04eb8722d0db07d7c91c44632b8
ayush singh <91491358+DeMoN-7@users.noreply.github.com>
2024-12-04 01:42:07 +0530

    commit

--- java/After Mid Sem/Exception Handling/CustomException1_1.java
+++ java/After Mid Sem/Exception Handling/CustomException1_1.java
@@ -1,3 +1,22 @@
+class Agee extends Exception{
+    Agee(String message){
+        super(message);
+    }
+}
 public class CustomException1_1 {
-    
+public static void main(String[] args) {
+    int a=10;
+    try{
+
+        if (a<11){
+            throw new Agee("Too Small");
+        }
+        else{
+            System.out.println("good");
+        }
+    }catch(Agee e){
+        System.out.println(e);
+    }
+
+}    
 }
